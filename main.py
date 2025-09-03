7110629768a1c35955081dc0c4fcf74c46e87699
eduardoreyes007351208 <lalosoccer307@gmail.com>
2025-09-03 14:29:07 -0700

    feat: Add URL validation, makes sure that the user inputs a valid URL to get data, if user enters 'q' it quits the program, and if any other string then lets the user know it's invalid URL and runs main.py again.

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from utils import get_data
+import validators
 
 def main():
     array = []
-    url = input('Enter the url for the Preppy Kitchen recipe: ')
-    new_array = get_data(url, array)
+    url = input('Enter the url for the Preppy Kitchen recipe(enter "q" to quit): ')
     
-    print(new_array)
-
+    if validators.url(url):
+        print('valid URL')
+        new_array = get_data(url, array)
+        print(new_array)
+        
+    else:
+        if (url.lower() == 'q'):
+            print('Goodbye!')
+        else:
+            print('Invalid URL')
+            main()
+    
 
 if __name__ == "__main__":
     main()
