eae076f3190401bd9aeb80ccb7caa3a24a8386f1
eduardoreyes007351208 <lalosoccer307@gmail.com>
2025-09-04 19:01:33 -0700

    fix: Get data from all forms of recipe websites
    Implement different statements that check for how
    the website stores its application/ld+json script
    and gets the data for most of them so far. Includes
    dict and list, @type='Recipe', or @group

--- utils/get_data.py
+++ utils/get_data.py
@@ -8,26 +8,58 @@ def get_data(url, arr):
     page = requests.get(url, headers=headers)
     soup = BeautifulSoup(page.content, 'html.parser')
     
-    title = soup.find('h2', class_='wprm-recipe-name').get_text()
-    file_name = soup.find('h2', class_='wprm-recipe-name').get_text().lower().replace(' ', '_')
-    equipment = soup.find('ul', class_='wprm-recipe-equipment')
-    ingredients = soup.find_all('ul', class_='wprm-recipe-ingredients')
-    instructions = soup.find_all('ul', class_='wprm-recipe-instructions')
+    #title = soup.find('h2', class_='wprm-recipe-name').get_text()
+    #file_name = soup.find('h2', class_='wprm-recipe-name').get_text().lower().replace(' ', '_')
+    #equipment = soup.find('ul', class_='wprm-recipe-equipment')
+    #ingredients = soup.find_all('ul', class_='wprm-recipe-ingredients')
+    #nstructions = soup.find_all('ul', class_='wprm-recipe-instructions')
+    complete_data = soup.find('script', type='application/ld+json').get_text()
+    group_array = json.loads(complete_data)
+    title = ''
+    equipment_list = ''
+    ingredient_list = ''
+    instruction_list = ''
     
-    
-    arr.append(title)
-    arr.append('Equipment:')
-    for child in equipment:
-        arr.append(child.get_text())
-    arr.append('Ingredients:')
-    for child in ingredients:
-        for c in child.children:
-            arr.append(c.get_text())
-    arr.append('Instructions')
-    for child in instructions:
-        for c in child.children:
-            arr.append(f'• {c.get_text()}')
+    if isinstance(group_array, dict):
+        graph_list = group_array.get('@graph', [])
+        if graph_list:
+            for items in graph_list:
+                types = items.get('@type')
+                if types == 'Recipe':
+                    recipe = items
+                    title = recipe.get('name')
+                    ingredient_list = recipe.get('recipeIngredient')
+                    instruction_list = recipe.get('recipeInstructions')
+        else:
+            types = group_array.get('@type')
+            if types == 'Recipe':
+                recipe = group_array
+                title = recipe.get('name')
+                ingredient_list = recipe.get('recipeIngredient')
+                instruction_list = recipe.get('recipeInstructions')
+    else:
+        print(False)
+        data = group_array[0]
+        title = data.get('name')
+        ingredient_list = data.get('recipeIngredient')
+        instruction_list = data.get('recipeInstructions')
         
+    print(f"{title}\n, {ingredient_list}\n, {instruction_list}")
+                    
+    
+   # arr.append(title)
+   # arr.append('Equipment:')
+   # for child in equipment:
+   #     arr.append(child.get_text())
+   # arr.append('Ingredients:')
+   # for child in ingredients:
+   #     for c in child.children:
+   #         arr.append(c.get_text())
+   # arr.append('Instructions')
+   # for child in instructions:
+   #     for c in child.children:
+   #         arr.append(f'• {c.get_text()}')
+    file_name = ''     
     return arr, file_name
 
 # try and add just @type == 'Recipe' if website has no @graph - ER 9/3/2025
@@ -38,12 +70,28 @@ def practice():
     soup = BeautifulSoup(page.content, 'html.parser')
     fullInfo = soup.find('script', type='application/ld+json').get_text()
     group_array = json.loads(fullInfo)
-    graph_list = group_array.get('@graph', [])
-    for items in graph_list:
-        types = items.get('@type')
-        if types == 'Recipe':
-            recipe = items
-            print(recipe.get('recipeIngredient'))
+    #graph_list = group_array.get('@graph', [])
+    if isinstance(group_array, dict):
+        graph_list = group_array.get('@graph', [])
+        if graph_list:
+            for items in graph_list:
+                types = items.get('@type')
+                if types == 'Recipe':
+                    recipe = items
+                    print(recipe.get('recipeIngredient'))
+        else:
+            types = group_array.get('@type')
+            if types == 'Recipe':
+                recipe = group_array
+                print(recipe.get('recipeIngredient'))
+    else:
+        print(False)
+        data = group_array[0]
+        print(data.get('recipeIngredient'))
+        
+        
+    #graph_list = group_array.get('@graph', [])
+    
     practice()
 if __name__ == '__main__':
     practice()
