b68fdcfdf4f2decc083dc32327bec49484e58645
eduardoreyes007351208 <lalosoccer307@gmail.com>
2025-09-06 17:12:30 -0700

    fix: Iterate through Ingredient and Instructions
    The script iterates through the list for the
    ingredients and the instructions, and appends to
    array.

--- utils/get_data.py
+++ utils/get_data.py
@@ -8,15 +8,9 @@ def get_data(url, arr):
     page = requests.get(url, headers=headers)
     soup = BeautifulSoup(page.content, 'html.parser')
     
-    #title = soup.find('h2', class_='wprm-recipe-name').get_text()
-    #file_name = soup.find('h2', class_='wprm-recipe-name').get_text().lower().replace(' ', '_')
-    #equipment = soup.find('ul', class_='wprm-recipe-equipment')
-    #ingredients = soup.find_all('ul', class_='wprm-recipe-ingredients')
-    #nstructions = soup.find_all('ul', class_='wprm-recipe-instructions')
-    complete_data = soup.find('script', type='application/ld+json').get_text()
+    complete_data = soup.find('script', type='application/ld+json').get_text().strip()
     group_array = json.loads(complete_data)
     title = ''
-    equipment_list = ''
     ingredient_list = ''
     instruction_list = ''
     
@@ -44,54 +38,26 @@ def get_data(url, arr):
         ingredient_list = data.get('recipeIngredient')
         instruction_list = data.get('recipeInstructions')
         
-    print(f"{title}\n, {ingredient_list}\n, {instruction_list}")
-                    
+    file_name = title.lower().replace(' ', '_')
+    arr.append(title)
+    print(f"\n{title}\n")
     
-   # arr.append(title)
-   # arr.append('Equipment:')
-   # for child in equipment:
-   #     arr.append(child.get_text())
-   # arr.append('Ingredients:')
-   # for child in ingredients:
-   #     for c in child.children:
-   #         arr.append(c.get_text())
-   # arr.append('Instructions')
-   # for child in instructions:
-   #     for c in child.children:
-   #         arr.append(f'• {c.get_text()}')
-    file_name = ''     
+    arr.append('\nIngredients:')
+    print('\nIngredients:')
+    for i, item in enumerate(ingredient_list, start=1):
+        arr.append(f'{i}. {item}')
+        print(f"{i}. {item}")
+    
+    arr.append('\nInstructions:')
+    print('\nInstructions:')
+    for section in instruction_list:
+        sec_name = section.get('name')
+        arr.append(f'{sec_name}')
+        print(f"\n{sec_name}")
+        
+        steps = section.get('itemListElement', [])
+        for i, step in enumerate(steps, start=1):
+            arr.append(f'{i}. {step.get('text')}')
+            print(f"{i}. {step.get('text')}")
+                      
     return arr, file_name
-
-# try and add just @type == 'Recipe' if website has no @graph - ER 9/3/2025
-def practice():
-    user_input = input('Enter the url for the Preppy Kitchen recipe(enter "q" to quit): ')
-    headers = {'User-Agent':'Mozilla/5.0 (X11; Linux x86_64; rv:137.0) Gecko/20100101 Firefox/137.0'}
-    page = requests.get(user_input, headers=headers)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    fullInfo = soup.find('script', type='application/ld+json').get_text()
-    group_array = json.loads(fullInfo)
-    #graph_list = group_array.get('@graph', [])
-    if isinstance(group_array, dict):
-        graph_list = group_array.get('@graph', [])
-        if graph_list:
-            for items in graph_list:
-                types = items.get('@type')
-                if types == 'Recipe':
-                    recipe = items
-                    print(recipe.get('recipeIngredient'))
-        else:
-            types = group_array.get('@type')
-            if types == 'Recipe':
-                recipe = group_array
-                print(recipe.get('recipeIngredient'))
-    else:
-        print(False)
-        data = group_array[0]
-        print(data.get('recipeIngredient'))
-        
-        
-    #graph_list = group_array.get('@graph', [])
-    
-    practice()
-if __name__ == '__main__':
-    practice()
