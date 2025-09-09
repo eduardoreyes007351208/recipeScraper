5395df0d0692282fe6266abf07998771b891d2b4
eduardoreyes007351208 <lalosoccer307@gmail.com>
2025-09-08 17:00:04 -0700

    fix: Update pyproject.toml and README for new build

--- recipescraper/utils/write_data.py
+++ recipescraper/utils/write_data.py
@@ -1,17 +1,6 @@
 from pathlib import Path
-import os
 
 def write_data(arr, file_name):
-    #file_path = Path(f'./txt_files/{file_name}.txt')
-    
-    #if file_path.exists():
-     #   print(f'\nTxt file of the {file_name} recipe already exist.\n')
-    #else:
-     #   print(f'\nGenerating txt for {file_name} recipe...\n')
-      #  for val in arr:
-       #     with open(f'./txt_files/{file_name}.txt', 'a') as f:
-        #        f.write(f'{val}\n')
-        
     current_dir = Path.cwd()
     file_path = Path(f'{current_dir}/{file_name}.txt')
          
