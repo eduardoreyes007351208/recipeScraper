ed4eb38370d6a67e1e9dbdc6a3d47d8280dfd31a
eduardoreyes007351208 <lalosoccer307@gmail.com>
2025-10-02 16:27:22 -0700

    fix: Add DejaVuSans font for unicode

--- recipescraper/main.py
+++ recipescraper/main.py
@@ -1,5 +1,5 @@
 # import libraries and modules for main.pu
-from .utils import get_data, write_data
+from .utils import get_data, pdf_generate
 import validators
 import argparse
 
@@ -21,7 +21,7 @@ def get_pdf(url: str):
             raise HTTPException(status_code=404, detail='Recipe not found')
             
         else:
-            pdf_bytes = bytes(write_data(new_array, title))
+            pdf_bytes = bytes(pdf_generate(new_array, title))
             return Response(
                 content=pdf_bytes,
                 media_type='application/pdf',
@@ -30,33 +30,7 @@ def get_pdf(url: str):
 
 # define the main functions
 def main():
-    # define array to contain data from get_data
-    array = []
-    # parser that parses arguments received from the user when called as a package, url
-    parser = argparse.ArgumentParser(description='Scrape cooking recipe from a website and write to a txt file.')
-    parser.add_argument('url', help='URL for the recipe page.')
-    url = parser.parse_args()
-    print(f'Scraping the recipe from {url.url}')
-    
-    # check to see the url argument from user is a valid url
-    if validators.url(url.url):
-        
-        new_array, file_name = get_data(url.url, array)
-        
-        if len(new_array) < 4:
-            print(f'I was not able to get the recipe from {url.url}')
-            
-        else:
-            write_data(new_array, file_name)
-            
-      
-    else:
-        # check to see if the invalid url is a 'q' to quit or other 
-        # invalid url that lets user know input was invalid
-        if (url.lower() == 'q'):
-            print('\nGoodbye!\n')
-        else:
-            print('\nInvalid URL\n')
+    pass
             
     
 
--- recipescraper/utils/pdf_generate.py
+++ recipescraper/utils/pdf_generate.py
@@ -3,20 +3,20 @@ from pathlib import Path
 from fpdf import FPDF
 
 # define function/module and define parameters 'arr' and 'file_name'
-def write_data(arr, title):
+def pdf_generate(arr, title):
     
-    font_path = Path(__file__).parent.parent / 'fonts' / 'Futura.ttf' 
+    font_path = Path(__file__).parent.parent / 'fonts' / 'DejaVuSansCondensed.ttf' 
     pdf = FPDF()
     pdf.set_left_margin(12)
     pdf.set_top_margin(20)  
     pdf.set_right_margin(12)
     pdf.set_auto_page_break(True, margin=20) 
     pdf.add_page() 
-    pdf.add_font('Futura', '', str(font_path))
-    pdf.set_font('Futura', size=24)
+    pdf.add_font('DejaVu', '', str(font_path), uni=True)
+    pdf.set_font('DejaVu', size=24)
     pdf.cell(pdf.epw, None, title.title(), align='C')
     pdf.ln(10)
-    pdf.set_font('Futura', size=12)
+    pdf.set_font('DejaVu', size=12)
     
     for val in arr: 
         if val == 'Ingredients:' or val == 'Instructions:':
